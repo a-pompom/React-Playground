1ba53181b02aeb4221ea001aa93ddbd39b14657d
a-pompom <43694794+a-pompom@users.noreply.github.com>
2021-08-11 13:57:59 +0900

    Props学習記事の土台作成

---
+++ chapter2-props/app/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const isVanilla = true;
+const entryPath = isVanilla ? './srcVanilla/index.ts' : './src/index.tsx';
+
+module.exports = {
+    /* ビルドオプション */
+    mode: 'development',
+
+    /* 入力 */
+    entry: entryPath,
+    /* 出力 */
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+
+    /* 開発サーバ */
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        host: 'localhost',
+        port: 30000
+    },
+
+    /* webpackがバンドルするモジュールをどう扱うか */
+    module: {
+        /* loaderをモジュールへ適用するための規則 */
+        rules: [
+            {
+                /* .ts or .tsxを対象 */
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    /* モジュールを読み込むとき、どう解決するか */
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    }
+};
+
